e76a789f5ce6e5fc4452d0602835558de6c82458
Harrison <Harrison.couto@outlook.com>
2022-12-15 19:37:27 -0300

    Criando componente do cabeçalho e fazendo as rotas

---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+
+export default class Header extends Component {
+  state = {
+    nameUser: '',
+    loading: true,
+  };
+
+  componentDidMount() {
+    this.showUserName();
+  }
+
+  showUserName = async () => {
+    const { name } = await getUser();
+    this.setState(
+      { loading: false, nameUser: name },
+    );
+  };
+
+  render() {
+    const { loading, nameUser } = this.state;
+    if (loading) {
+      return <span>Carregando...</span>;
+    }
+    return (
+      <header data-testid="header-component">
+        <h2 data-testid="header-user-name">{ nameUser }</h2>
+        <ul>
+          <li>
+            <Link to="/">Login</Link>
+          </li>
+          <li>
+            <Link data-testid="link-to-search" to="/search">Search</Link>
+          </li>
+          <li>
+            <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+          </li>
+          <li>
+            <Link data-testid="link-to-profile" to="/album">Album</Link>
+          </li>
+        </ul>
+      </header>
+    );
+  }
+}
